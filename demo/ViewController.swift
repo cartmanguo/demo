15c92846b399e5247eb6b7dc00345d93f72e75a7
randy <randy@randydeMac-mini-3.local>
2015-06-01 10:03:27 +0800

    demo

--- demo/ViewController.swift
+++ demo/ViewController.swift
@@ -8,11 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UITextFieldDelegate {
 
+    @IBOutlet weak var hoshiTextField: HoshiTextField!
+    var datePicker:UIDatePicker!
     override func viewDidLoad() {
         super.viewDidLoad()
+        datePicker = UIDatePicker()
+        datePicker.datePickerMode = UIDatePickerMode.DateAndTime
+        hoshiTextField.inputView = datePicker
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let str = dateFormatter.stringFromDate(datePicker.date)
+        hoshiTextField.text = str
     }
 
     override func didReceiveMemoryWarning() {
